ca49833a7faf0d1ef898f9e9b48acd672a824409
Enmanuel <duranenmanuel@gmail.com>
2018-09-23 17:59:17 -0300

    Fixing css duplication with gatsby-browser

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,6 @@ import Footer from 'components/Footer/Footer';
 import Favicon16 from 'images/favicon-16x16.png';
 import Favicon32 from 'images/favicon-32x32.png';
 import AppleTouchIcon from 'images/apple-touch-icon.png';
-import './styles.scss';
 
 const Layout = ({ children, section, classes }) => {
     return (
