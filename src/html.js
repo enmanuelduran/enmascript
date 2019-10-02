1694bbddee1afcddb6c059ab7330671e46de01a0
Enmanuel Durán <duranenmanuel@gmail.com>
2019-10-01 23:06:59 -0300

    Google ads second test

--- src/html.js
+++ src/html.js
@@ -27,6 +27,7 @@ export default function HTML(props) {
                 <script
                     async
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <script dangerouslySetInnerHTML={{__html: '(adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "ca-pub-4858256894602983", enable_page_level_ads: true });'}}></script>
             </body>
         </html>
     );
